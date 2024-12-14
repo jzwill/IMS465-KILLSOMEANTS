5425a7908b457ed5554a47de6bde0efea2f297e0
jzwill <zwillijt@miamioh.edu>
2024-12-14 08:02:41 -0500

    mainMenuAdded

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -9,10 +9,32 @@ public class Menu : MonoBehaviour
     [SerializeField] Animator anim;
 
     private bool isMenuOpen = true;
+    private bool isGamePaused = false;
+    private bool fastForwardOn = false;
 
     public void ToggleMenu() {
         isMenuOpen = !isMenuOpen;
         anim.SetBool("MenuOpen", isMenuOpen);
+    }
+
+    public void PauseGame() {
+        if (!isGamePaused){
+            isGamePaused = true;
+            Time.timeScale = 0;
+        } else {
+            isGamePaused = false;
+            Time.timeScale = 1;
+        }
+    }
+
+        public void FastForward() {
+        if (!fastForwardOn){
+            fastForwardOn = true;
+            Time.timeScale = 2;
+        } else {
+            fastForwardOn = false;
+            Time.timeScale = 1;
+        }
     }
 
     private void OnGUI() {
